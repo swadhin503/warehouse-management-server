617508f9c398b28af4d66631345980e40e9d336d
swadhin503 <tawhiddwinswadhin@gmail.com>
2022-05-07 00:55:55 +0600

    all items are shown

--- index.js
+++ index.js
@@ -13,13 +13,24 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcnkg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('connected');
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try {
+        await client.connect();
+        const itemCollection = client.db('warehouse_inventory').collection('items');
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req,res) => {
     res.send('running server');
